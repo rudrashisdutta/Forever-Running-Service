1d1f531936a99805b160497ece347980f60eed70
rudrashisdutta <rudra2001official@gmail.com>
2021-07-12 21:52:21 +0530

    Initial commit
    12-07-2021 09-52p

---
+++ app/src/main/java/com/android/foreverservice/MainActivity.java
@@ -0,0 +1,20 @@
+package com.android.foreverservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            startService(new Intent(getApplicationContext(), MyService.class));
+        });
+    }
+}
